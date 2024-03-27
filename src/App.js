05b66502108562824936c0418a12a7b1178e7126
Dago Salas <dagosalas1999@gmail.com>
2024-03-26 19:07:34 -0600

    last commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar.jsx";
 import Hero from "./Components/Hero.jsx";
 export default function App() {
     return (
-        <>
+        <>   <div className="block"></div>
             <Navbar />
             <Hero />
         </>
